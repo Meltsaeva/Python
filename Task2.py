adf91566e17a2bacd56af008544e5d96ba6c1b77
Anastasiia <Batrakovaad@yndex.ru>
2023-04-09 12:10:16 +0700

    check

--- Task2.py
+++ Task2.py
@@ -3,7 +3,10 @@
 # 100 -> 1 (1 + 0 + 0)
 
 num = int(input("Input a number - "))
-a = num % 10 
-b = num % 100 //10
-c = num // 100
-print(f" {a + b + c} ({c} + {b} + {a})")
+if 99 < num < 1000:
+    a = num % 10 
+    b = num % 100 //10
+    c = num // 100
+    print(f" {a + b + c} ({c} + {b} + {a})")
+else:
+    print("Incorrect")
