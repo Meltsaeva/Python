388f8f5268c3bbc5369ab4edcf78f2cd81259013
Anastasiia <Batrakovaad@yndex.ru>
2023-04-18 12:44:40 +0700

    finish21

--- Task21.py
+++ Task21.py
@@ -4,3 +4,10 @@
 # {"VI": "S005"}, {"VII": " S005 "}, {" V ":" S009 "}, {" VIII
 # ":" S007 "}]
 # Output: {'S005', 'S002', 'S007', 'S001', 'S009'}
+
+dictionary = [{"V": "S001"}, {"V": "S002"}, {"VI": "S001"}, {"VI": "S005"}, {"VII": "S005"}, {" V ":"S009"}, {" VIII":"S007"}]
+list = []
+for i in dictionary:
+    for j in i:
+        list.append(i[j])
+print(set(list))
