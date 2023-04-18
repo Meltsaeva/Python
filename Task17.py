4d224cc3d0aa38188725b31b8b40458a268f71e3
Anastasiia <Batrakovaad@yndex.ru>
2023-04-18 12:28:57 +0700

    finish19

--- Task17.py
+++ Task17.py
@@ -4,11 +4,12 @@
 # Output: 6
 
 
+
 n = int(input("Input the number of elements - "))
 list = list()
 for i in range(n):
     x = int(input("Input x - "))
     list.append(x)
-list_1 = set(list)
-sum = len(list_1)
+sum = len(set(list))
 print(sum)
+
--- Task19.py
+++ Task19.py
@@ -4,3 +4,15 @@
 # положительное число.
 # Input: [1, 2, 3, 4, 5] k = 3
 # Output: [4, 5, 1, 2, 3]
+
+k = int(input("Input K - "))
+n = int(input("Input N - "))
+list = []
+for i in range(n):
+    x = int(input("Input x - "))
+    list.append(x)
+print(list)
+for i in range(k):
+    list.insert(0, list.pop())
+# print(*list[-k:], *list[:len(list) - k])
+print(*list)
